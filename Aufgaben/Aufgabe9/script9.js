b34fd047cfa2cc3c411c92d1bddaedf1fc657a7a
baslerja <63954888+baslerja@users.noreply.github.com>
2020-06-28 10:42:44 +0200

    Aufgabe 9 hochgeladen

---
+++ Aufgaben/Aufgabe9/script9.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', function () {
+    var todolist = ["Get snacks for New Year's Eve", "Visit the family", "Complete this EIA prototype"];
+    //To Do Liste erstellen
+    function createList() {
+        todo.innerHTML = "";
+        for (var index = 0; index < todolist.length; index++) {
+            todo.innerHTML += "<div>" + "<input type='checkbox'>" + todolist[index] + "<i class='far fa-trash-alt'></i>" + "</div>";
+        }
+        var total = document.querySelector("#counter");
+        total.innerHTML = todolist.length;
+    }
+    var todo = document.querySelector(".addlist");
+    createList();
+    //Add new task
+    var input = document.querySelector("#newtask");
+    var addTask = document.querySelector("#addbutton");
+    addTask.addEventListener('click', function () {
+        todolist.push(input.value);
+        createList();
+        input.value = "";
+    });
+    //Delete
+    var close = document.getElementsByClassName("far fa-trash-alt");
+    var i;
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        };
+    }
+});
+//# sourceMappingURL=script9.js.map
